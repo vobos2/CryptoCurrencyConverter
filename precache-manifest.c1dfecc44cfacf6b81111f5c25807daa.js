6ebf17056b1be7dcf3ffe242119c948394775728
Alexsandr Vobornov <vobos2@gmail.com>
2020-08-08 23:18:23 -0400

    Updates

---
+++ precache-manifest.c1dfecc44cfacf6b81111f5c25807daa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901ea4b79317ebd68478563d700ce94d",
+    "url": "/CryptoCurrencyConverter/index.html"
+  },
+  {
+    "revision": "5a15ffb2532fe975cdf4",
+    "url": "/CryptoCurrencyConverter/static/css/main.0a726ba0.chunk.css"
+  },
+  {
+    "revision": "e3e23bdf0bbbea421fa9",
+    "url": "/CryptoCurrencyConverter/static/js/2.c3587b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CryptoCurrencyConverter/static/js/2.c3587b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a15ffb2532fe975cdf4",
+    "url": "/CryptoCurrencyConverter/static/js/main.31f38586.chunk.js"
+  },
+  {
+    "revision": "6565b3dd9c7d462363c6",
+    "url": "/CryptoCurrencyConverter/static/js/runtime-main.6b423a15.js"
+  },
+  {
+    "revision": "9caf06ff0fa09e55b6b6b2b553f74159",
+    "url": "/CryptoCurrencyConverter/static/media/0.9caf06ff.jpg"
+  },
+  {
+    "revision": "d9251295f8257c066acf4406ce89bf13",
+    "url": "/CryptoCurrencyConverter/static/media/1.d9251295.jpg"
+  },
+  {
+    "revision": "e4d504f14f57f3ecad76d28e10512ad4",
+    "url": "/CryptoCurrencyConverter/static/media/10.e4d504f1.jpg"
+  },
+  {
+    "revision": "07cb3dab97a0b1c36b827c0e20203010",
+    "url": "/CryptoCurrencyConverter/static/media/11.07cb3dab.jpg"
+  },
+  {
+    "revision": "9f476dc35b88f21731eedaa2c607bf00",
+    "url": "/CryptoCurrencyConverter/static/media/12.9f476dc3.jpg"
+  },
+  {
+    "revision": "b33ca6bcff5c1145b79b4715001da4a0",
+    "url": "/CryptoCurrencyConverter/static/media/13.b33ca6bc.jpg"
+  },
+  {
+    "revision": "0a611787c01ee34e7b37ebbff09210de",
+    "url": "/CryptoCurrencyConverter/static/media/14.0a611787.jpg"
+  },
+  {
+    "revision": "807e65faca50558709538ed6f0fbe4f1",
+    "url": "/CryptoCurrencyConverter/static/media/15.807e65fa.jpg"
+  },
+  {
+    "revision": "1a5aab7c3cc0537edee8382628fc6fc2",
+    "url": "/CryptoCurrencyConverter/static/media/16.1a5aab7c.jpg"
+  },
+  {
+    "revision": "64c087a83fa6d83e9f61a9aab6e53834",
+    "url": "/CryptoCurrencyConverter/static/media/17.64c087a8.jpg"
+  },
+  {
+    "revision": "d11da072faaea951b7708f77c0d92369",
+    "url": "/CryptoCurrencyConverter/static/media/18.d11da072.jpg"
+  },
+  {
+    "revision": "946e8c1a3c9f4326a388798532936934",
+    "url": "/CryptoCurrencyConverter/static/media/19.946e8c1a.jpg"
+  },
+  {
+    "revision": "02a91e252ade209569558299801bd4bf",
+    "url": "/CryptoCurrencyConverter/static/media/2.02a91e25.jpg"
+  },
+  {
+    "revision": "d8c00df783a67fbc80b30bc47ce6ca22",
+    "url": "/CryptoCurrencyConverter/static/media/3.d8c00df7.jpg"
+  },
+  {
+    "revision": "6bcb1e6cefe7fe3764c7e51a663d0d6f",
+    "url": "/CryptoCurrencyConverter/static/media/4.6bcb1e6c.jpg"
+  },
+  {
+    "revision": "b2f07badf3d83f10ee3efcc2d4ad8a15",
+    "url": "/CryptoCurrencyConverter/static/media/5.b2f07bad.jpg"
+  },
+  {
+    "revision": "8427639bfa3e409f3adbdf25c72b36a1",
+    "url": "/CryptoCurrencyConverter/static/media/6.8427639b.jpg"
+  },
+  {
+    "revision": "ecb1b27a79c7c86b06fe6a881ef9fc39",
+    "url": "/CryptoCurrencyConverter/static/media/7.ecb1b27a.jpg"
+  },
+  {
+    "revision": "6c58a740cf6be9900f3463621f34ae3e",
+    "url": "/CryptoCurrencyConverter/static/media/8.6c58a740.jpg"
+  },
+  {
+    "revision": "ddb7f3fc56df255f41772f60fe1b5c19",
+    "url": "/CryptoCurrencyConverter/static/media/9.ddb7f3fc.jpg"
+  }
+]);
